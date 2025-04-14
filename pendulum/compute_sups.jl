bb0e9edcca3cb799daf7dfde58052fcfa82004e9
HuggyHugMe <chugodu92@gmail.com>
2025-04-14 17:47:31 +0100

    fix stuff for the pendulum example

--- pendulum/compute_sups.jl
+++ pendulum/compute_sups.jl
@@ -3,7 +3,6 @@ using Polynomials, PolynomialRoots, IntervalArithmetic, Combinatorics, Serializa
 prec = 2048
 setprecision(prec)
 
-M0 = big(450)
 M = big(550)
 
 z = Polynomial(interval.(BigFloat, [0, 1]))
@@ -16,22 +15,7 @@ sups[1] = interval(BigFloat, 1.0)
 
 ϵ  = 2^(-prec//2)
 
-for m = 1:M0+1
-    println(m)
-    # DH = interval(BigFloat, 2*m)*Hprev - z*H
-    # X = sort(real.(PolynomialRoots.roots(mid.(DH.coeffs))))
-    # Xl = interval.(X .- ϵ)
-    # Xu = interval.(X .+ ϵ)
-    # if all(sign.(DH.(Xl)).*sign.(DH.(Xu)) .== -1) && all(sup.(Xl[1:end-1]).< inf.(Xu[2:end])) && length(X) == m + 1
-    #     Xrig = interval.(inf.(Xl), sup.(Xu))
-    #     sups[m+1] = interval(Float64, interval(BigFloat,maximum(sup.(abs.(H.(Xrig)).*exp.(-Xrig.^2/interval(2)))))/sqrt(interval(BigFloat, 2^m*factorial(m))))
-    # else
-    #     println("problem")
-    # end
-    H, Hprev = interval(2)*z*H - interval(BigFloat, 2*m)*Hprev, H
-end
-
-for m = M0+2:M+1
+for m = 1:M+1
     println(m)
     DH = interval(BigFloat, 2*m)*Hprev - z*H
     X = sort(real.(PolynomialRoots.roots(mid.(DH.coeffs))))
@@ -45,7 +29,5 @@ for m = M0+2:M+1
     end
     H, Hprev = interval(2)*z*H - interval(BigFloat, 2*m)*Hprev, H
 end
-
-# sups[1:M0+2] = deserialize("pendulum_sups")
 
 serialize("pendulum_sups", sups)
